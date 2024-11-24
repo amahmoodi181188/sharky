0445ad308e25ef95a56864fcde850e0a40502edb
Ali Mahmoodi <alimahmoodi@Alis-Air.fritz.box>
2024-11-24 19:28:31 +0100

    edit main.go

--- app1/main.go
+++ app1/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "There's no such thing as a bad day when you're fishing.")
+	fmt.Fprintf(w, "There's no such thing as a bad day when you're fishing.")
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
 
--- app2/main.go
+++ app2/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "When in doubt, fish!")
+	fmt.Fprintf(w, "When in doubt, fish!")
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
 
